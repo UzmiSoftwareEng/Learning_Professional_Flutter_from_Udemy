4ee86a84d352af0d99c5519b9ee4dd078d5d0194
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-09-22 09:41:02 +0500

    Adding color in container for making Ui attractive

--- day_5/lib/main.dart
+++ day_5/lib/main.dart
@@ -4,19 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home:Scaffold(
-        body: Center(
-          child: Container(
-              width: 200,
-            height: 200,
-            color: Colors.purple,
-              child: Center(
-                  child: Text("Hello Developers",
-                  style: TextStyle(fontSize: 21,
-                      color: Colors.white),),
-              ),
-            ),
-        ),
-        ),
+        body: GradientWidget(),
       ),
+    ),
   );
+      }
+class GradientWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    width: 200,
+    height: 200,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(200, 150, 20, 80),
+            Color.fromARGB(200, 150, 20, 80),
+            ],
+              begin: Alignment.bottomRight,
+              end: Alignment.topRight
+          ),),
+          child: Text("Hello Developers",
+          style: TextStyle(fontSize: 21,
+          color: Colors.white),
+          ),
+    );
+  }
+
 }
